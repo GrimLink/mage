adcf167779fd646f948a1734860751aec837ddc8
grimlink <sean.grimlink@gmail.com>
2024-02-25 21:10:45 +0100

    IMP: bundling to keep it simple

--- src/build.sh
+++ src/build.sh
@@ -1,26 +1,26 @@
-function mage_build() {
-  local LANGS="en_US nl_NL"
-  local JOBS=4
-  local FORCE=""
-  local DEFAULT_ARGS="$LANGS $JOBS"
-  local ARGS=${@}
+#!/bin/bash
 
-  # If language is not found in ARGS add the default
-  if [[ $ARG != *_* ]]; then
-    local ARGS="$LANGS $ARGS"
-  fi
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+sorted_files=("_global")
+files=("${script_dir}"/_*)
 
-  # If Jobs is not found in ARGS add the default
-  if [[ "$ARGS" != *"-j"* ]] || [[ "$ARGS" != *"--jobs"* ]]; then
-    local ARGS="$ARGS -j $JOBS"
-  fi
+for file in "${files[@]}"; do
+  [[ "${file}" == "${script_dir}"/_mage ]] || sorted_files+=("${file#${script_dir}/}")
+done
 
-  # If force is found in ARGS add it to both adminhtml and frontend
-  if [[ "$ARGS" == *"-f"* ]] || [[ "$ARGS" == *"--force"* ]]; then
-    local FORCE="-f"
-  fi
+# Add _mage to the end
+sorted_files+=("_mage")
 
-  # Deploy static content
-  $MAGENTO_CLI setup:static-content:deploy -a adminhtml en_US ${FORCE} &&
-  $MAGENTO_CLI setup:static-content:deploy -a frontend ${ARGS:-$DEFAULT_ARGS}
+function merge_files() {
+  echo -e "#!/bin/bash\n"
+  echo "# Mage is a collection of easy commands and aliases for bin/magento"
+  echo -e "# For those who hate typing long shell commands\n"
+
+  for file in "$@"; do
+    cat "${script_dir}/${file}"
+    echo
+  done
 }
+
+merge_files "${sorted_files[@]}" > ../mage
+chmod +x ../mage
