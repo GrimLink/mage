cdc4c51600b84d1ea9a77cea8e6e0cb1759ae10f
Sean van Zuidam <sean.grimlink@gmail.com>
2026-01-10 13:09:03 +0100

    FIX: setup var

--- src/_setup.sh
+++ src/_setup.sh
@@ -131,7 +131,7 @@ function mage_setup() {
 }
 
 function mage_getting_started() {
-  local name=$2
+  local name=$1
   echo -e "$name is ready!"
   echo -e "Enter your Magento project directory using ${BLUE}cd ./${name}${RESET}"
   echo -e "Open your Magento project, using ${BLUE}mage open${RESET} or ${BLUE}mage open admin${RESET}"
